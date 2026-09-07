1232733292b619ccdaa6a0870096f1db808e7239
xuvictory <425723759@qq.com>
2026-09-07 14:49:03 +0800

    附加属性的用法示例

---
+++ WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+        }
+
+
+
+        public string MyPasswordVM
+        {
+            get { return (string)GetValue(MyPasswordVMProperty); }
+            set { SetValue(MyPasswordVMProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyPasswordVM.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MyPasswordVMProperty =
+            DependencyProperty.Register(nameof(MyPasswordVM), typeof(string), typeof(MainWindow));
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MyPasswordVM = DateTime.Now.ToString("HH:mm:ss ffff");
+        }
+
+    }
+}
---
+++ WpfApp6/PasswordBoxHelper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp6
+{
+    public class PasswordBoxHelper
+    {
+
+
+        public static string GetPwd(DependencyObject obj)
+        {
+            return (string)obj.GetValue(PwdProperty);
+        }
+
+        public static void SetPwd(DependencyObject obj, string value)
+        {
+            obj.SetValue(PwdProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for Pwd.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PwdProperty =
+            DependencyProperty.RegisterAttached("Pwd", typeof(string), typeof(PasswordBoxHelper), new PropertyMetadata("", OnPropertyChanedCallBack));
+
+        private static void OnPropertyChanedCallBack(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            PasswordBox pwdbox = d as PasswordBox;
+            if (pwdbox == null) return;
+
+            pwdbox.Password = (string)e.NewValue;
+        }
+
+
+
+        public static bool GetIsEnablePasswordPropertyChanged(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsEnablePasswordPropertyChangedProperty);
+        }
+
+        public static void SetIsEnablePasswordPropertyChanged(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsEnablePasswordPropertyChangedProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for IsEnablePasswordPropertyChanged.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsEnablePasswordPropertyChangedProperty =
+            DependencyProperty.RegisterAttached("IsEnablePasswordPropertyChanged", typeof(bool), typeof(PasswordBoxHelper), new PropertyMetadata(false, OnPasswordPropertyChanged));
+
+        private static void OnPasswordPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            PasswordBox pwdbox = d as PasswordBox;
+            if (pwdbox == null)
+                return;
+
+            if((bool)e.NewValue)
+            {
+                pwdbox.PasswordChanged += Pwdbox_PasswordChanged;
+            }
+            else
+            {
+                pwdbox.PasswordChanged -= Pwdbox_PasswordChanged;
+            }
+        }
+
+        private static void Pwdbox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            PasswordBox pwdbox = (PasswordBox)sender;
+
+            SetPwd(pwdbox, pwdbox.Password);
+        }
+    }
+}
